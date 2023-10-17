0c61511972e0b1d307b0843b2154793774e628b7
bhuvanesh <iambhuvanesh@gmail.com>
2023-10-17 21:27:01 +0530

    load balancer docker set up.

---
+++ nodeserver/server.js
@@ -0,0 +1,18 @@
+'use strict';
+ 
+const express = require('express');
+ 
+// Constants
+const PORT = 7070;
+const HOST = '0.0.0.0';
+ 
+// App
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the world of Node.Js');
+});
+ 
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
